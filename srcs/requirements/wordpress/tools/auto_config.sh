b4a95c53188d913d9a7d160b9eae4766fed0bcab
LLePep <palabostleo@gmail.com>
2026-06-24 22:44:25 +0200

    update

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -6,20 +6,23 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
 
     cd /var/www/html
 
+    wp core download --allow-root
+
 	wp config create	--allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
     --dbhost=mariadb:3306 --path='/var/www/html'
-    wp core install --url=https://localhost:443 \
-    --title=my_site \
-    --admin_user=random \
-    --admin_password=random \
-    --admin_email=adiosbahamas@gmail.com \
+    wp core install --url=https://$DOMAIN_NAME:443 \
+    --title=$WP_TITLE \
+    --admin_user=$WP_USER \
+    --admin_password=$WP_PASSWORD \
+    --admin_email=$WP_EMAIL \
     --allow-root \
     --skip-email
     wp user create username username@gmail.com \
     --allow-root \
+    --role=editor \
     --user_pass=username_password
 fi
 
